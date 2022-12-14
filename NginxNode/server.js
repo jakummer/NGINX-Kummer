c2c65ea17b76e0aefb066f0c2626727411798ca6
jakummer <jakummer@gmail.com>
2022-12-14 08:22:58 -0300

    Entrega Nginx server - JAKummer

--- NginxNode/server.js
+++ NginxNode/server.js
@@ -6,19 +6,17 @@ app.use(express.static('public'));
 
 const PORT = parseInt(process.argv[2]) || 8080;
 
-app.get('/api/random', (req, res) => {
+app.get('/api/randoms', (req, res) => {
     console.log( `La app se está conectando al servidor express Nginx en: ${PORT} :: ${Date.now()}`);
     res.send(`La app se está conectando al servidor express Nginx en ${PORT}`);
 
 });
 
 app.get('/test', (req, res) => {
-    console.log(`test Port: ${PORT} -> FyH: ${Date.now()}`);
-    res.send(`test Servidor express Nginx en ${PORT}`);
+    console.log(`Hacemos test en Port: ${PORT} :: ${Date.now()}`);
+    res.send(`Hacemos test Servidor express Nginx en ${PORT}`);
 });
 
 
 app.listen(PORT);
 
-// pm2 start server.js --name="Server1" --watch -- 8081
-// pm2 start server.js --name="Server2" --watch -i max -- 8082
